5717d111ce0c1375479c191042779e0846b2d4f4
Alexander Bricken <Alexander.Bricken@gmail.com>
2018-10-10 22:35:58 -0700

    edit text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,9 @@ const IndexPage = () => (
     <p>
     Have a look around, but make sure to check out my real website <a href="https://bricken.co">here</a>.
     </p>
+    <p>
+    Im just getting started... and cant wait to be at the FreeCodeCamp hackathon!
+    </p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
